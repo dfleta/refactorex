74adfde16e217d817fc32b29445cc709e204e34c
David Gelpi <gelpiorama@gmail.com>
2023-12-15 14:10:59 +0100

    fix(PinPals): Corregir atribucion autor cita

--- src/PinPals.java
+++ src/PinPals.java
@@ -4,7 +4,7 @@ import com.github.freva.asciitable.AsciiTable;
 public class PinPals {
     public static void main( String[] args ) {
 
-        System.out.println("\nSmokey, this is not Vietnam, this is bowling. There are rules. -The Dude\n");
+        System.out.println("\n\"Smokey, this is not Vietnam, this is bowling. There are rules.\" - Walter Sobchak\n");
 
         String[] headers = {"Frame 1", "Frame 2", "Frame 3", "Frame 4", "Frame 5", 
                             "Frame 6", "Frame 7", "Frame 8", "Frame 9", "Frame 10"};
